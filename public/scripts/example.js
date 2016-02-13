0418fc1fcaa270e1a45391ee0d2aab2673ccfe73
lookOnTheBrightSide <nkosindlovu87@gmail.com>
2016-02-13 21:48:17 +0200

    react starter

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,3 +1,17 @@
+var welcomeBox = React.createClass({
+	render: function(){
+		return(
+			React.createElement('p', {className: "welcomeBox"},
+				"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est dolore eum perspiciatis libero, unde ipsam. Ullam sequi, magnam provident commodi molestiae id laudantium. Ratione ea veniam sit vero, quam sed."
+				)
+			)
+	}
+})
+
+ReactDOM.render(React.createElement(welcomeBox,null),
+	document.getElementById('content')
+);
+
 
 var CommentBox = React.createClass({displayName: 'CommentBox',
   render: function() {
